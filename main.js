f34df69ab91bd72e077cb64e48174c26fc8cc9d0
Oaes Kuruni <oaeskuruni27@gmail.com>
2019-07-06 22:13:02 +0600

    added a file

---
+++ main.js
@@ -0,0 +1,15 @@
+// -------------(if-else condition in js)-----------------------------//
+
+var a = 2
+var b = 9
+ if (a>b){
+     console.log(a+b)
+ }
+  var n = 20
+  if (n%2 == 0){
+      console.log(n+ 'is even number')
+  }
+
+  if (n%2 ==1){
+      console.log(n+ 'is odd number');
+  }
